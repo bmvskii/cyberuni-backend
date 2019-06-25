bd3d22f7dd81c43f883d75439e8a02f47c064b32
Evgeniy Rogovoy <evg.rogovoy@gmail.com>
2019-06-26 02:24:57 +0300

    make some changes

--- config/db_settings.js
+++ config/db_settings.js
@@ -1,4 +1,5 @@
 module.exports = {
-  DB_URL: 'mongodb://localhost/',
+  // DB_URL: 'mongodb://localhost/',
+  DB_URL: 'mongodb+srv://bmvskii:08021998Evg@cyberuni-htotl.mongodb.net/test',
   HOST_URL: 'http://localhost:3000',
 }
